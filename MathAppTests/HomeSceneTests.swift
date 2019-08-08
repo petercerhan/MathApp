f5bd4f811247a46c701abc1cba578a1f4a7e3209
Peter Cerhan <peter@foliahealth.com>
2019-08-07 20:48:22 -0400

    remove extra files

---
+++ MathAppTests/HomeSceneTests.swift
@@ -0,0 +1,15 @@
+//
+//  HomeSceneTests.swift
+//  MathAppTests
+//
+//  Created by Peter Cerhan on 8/7/19.
+//  Copyright © 2019 Peter Cerhan. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import MathApp
+
+class HomeSceneTests: XCTestCase {
+    
+}
