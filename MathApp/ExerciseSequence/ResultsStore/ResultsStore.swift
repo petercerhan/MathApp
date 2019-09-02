e5ca7bf544142d6914671e368c0b2111cb1c591c
Peter Cerhan <peter@foliahealth.com>
2019-09-01 20:37:51 -0500

    Results store

---
+++ MathApp/ExerciseSequence/ResultsStore/ResultsStore.swift
@@ -0,0 +1,24 @@
+//
+//  ResultsStore.swift
+//  MathApp
+//
+//  Created by Peter Cerhan on 9/1/19.
+//  Copyright © 2019 Peter Cerhan. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol ResultsStore {
+    var correct: Observable<Int> { get }
+}
+
+extension ResultsStore where Self: ResultsStoreImpl {
+    var correct: Observable<Int> {
+        return correctSubject.asObservable()
+    }
+}
+
+class ResultsStoreImpl: ResultsStore {
+    let correctSubject = BehaviorSubject<Int>(value: 0)
+}
---
+++ MathAppTests/ExerciseSequence/ResultsStore/ResultsStoreTests.swift
@@ -0,0 +1,28 @@
+//
+//  ResultsStoreTests.swift
+//  MathAppTests
+//
+//  Created by Peter Cerhan on 9/1/19.
+//  Copyright © 2019 Peter Cerhan. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import RxSwift
+@testable import MathApp
+
+class ResultsStoreTests: XCTestCase {
+    
+    let disposeBag = DisposeBag()
+    
+    func test_correct_initialState_0() {
+        let store = ResultsStoreImpl()
+        
+        let correct = latestValue(of: store.correct, disposeBag: disposeBag) ?? -1
+        XCTAssertEqual(correct, 0)
+    }
+    
+    
+    
+    
+}
