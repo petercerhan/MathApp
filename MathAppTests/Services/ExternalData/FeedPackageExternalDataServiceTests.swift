9eb00e53fe2a9f62a201437cda2673b8bcb01f95
Peter Cerhan <peter@foliahealth.com>
2019-10-05 15:23:14 -0400

    Feed Sequence directory

---
+++ MathAppTests/Services/ExternalData/FeedPackageExternalDataServiceTests.swift
@@ -0,0 +1,27 @@
+//
+//  FeedPackageExternalDataServiceTests.swift
+//  MathAppTests
+//
+//  Created by Peter Cerhan on 10/5/19.
+//  Copyright © 2019 Peter Cerhan. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import MathApp
+
+class FeedPackageExternalDataServiceTests: XCTestCase {
+    
+    func test_getFeedPackage_allConceptsStrength0_shouldIntroduceConcept1() {
+        let stubUserConcepts = [UserConcept.constantRule, UserConcept.linearRule, UserConcept.powerRule, UserConcept.sumRule, UserConcept.differenceRule]
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
