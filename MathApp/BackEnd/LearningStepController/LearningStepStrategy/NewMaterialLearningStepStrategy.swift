105ac1b3e90d2746560cbfcced54f7f69d4fb850
Peter Cerhan <peter@foliahealth.com>
2019-10-20 17:09:45 -0400

    Refactoring for NewMaterialStepStrategy

--- MathApp/BackEnd/LearningStepController/LearningStepStrategy/NewMaterialLearningStepStrategy.swift
+++ MathApp/BackEnd/LearningStepController/LearningStepStrategy/NewMaterialLearningStepStrategy.swift
@@ -15,48 +15,56 @@ class NewMaterialLearningStepStrategy: LearningStepStrategy {
     private let userConceptRepository: UserConceptRepository
     private let newMaterialStateRepository: NewMaterialStateRepository
     
+    //MARK: - Context
+    
+    private let focus1ID: Int
+    private let focus2ID: Int
+    private var userConcept1: UserConcept? = nil
+    private var userConcept2: UserConcept? = nil
+    
     //MARK: - Initialization
     
     init(userConceptRepository: UserConceptRepository, newMaterialStateRepository: NewMaterialStateRepository) {
         self.userConceptRepository = userConceptRepository
         self.newMaterialStateRepository = newMaterialStateRepository
+        
+        let newMaterialLearningStep = newMaterialStateRepository.get()
+        focus1ID = newMaterialLearningStep.focusConcept1ID
+        focus2ID = newMaterialLearningStep.focusConcept2ID
+        findNextUserConcepts()
+    }
+    
+    private func findNextUserConcepts() {
+        let userConcepts = userConceptRepository.list()
+        
+        guard let userConcept1_index = userConcepts.firstIndex(where: { $0.strength == 0 || $0.strength == 1 }) else {
+            return
+        }
+        userConcept1 = userConcepts[userConcept1_index]
+        
+        var searchUserConcept2 = userConcepts
+        searchUserConcept2.remove(at: userConcept1_index)
+        if searchUserConcept2.count > 0 {
+            userConcept2 = searchUserConcept2.first(where: { $0.strength == 0 || $0.strength == 1 })
+        }
+        
     }
     
     //MARK: - LearningStepStrategy Interface
     
     func nextLearningStep() -> LearningStep {
-        let userConcepts = userConceptRepository.list()
-        let newMaterialLearningStep = newMaterialStateRepository.get()
-        let focus1ID = newMaterialLearningStep.focusConcept1ID
-        let focus2ID = newMaterialLearningStep.focusConcept2ID
-        
-        guard let userConcept1_index = userConcepts.firstIndex(where: { $0.strength == 0 || $0.strength == 1 }) else {
+        guard let userConcept1 = userConcept1 else {
             #warning("Should return practice family")
             return ConceptIntroLearningStep(conceptID: 1)
         }
         
-        //get user concepts
-        let userConcept1 = userConcepts[userConcept1_index]
-        var searchUserConcept2 = userConcepts
-        searchUserConcept2.remove(at: userConcept1_index)
-        var userConcept2: UserConcept?
-        if searchUserConcept2.count > 0 {
-            userConcept2 = searchUserConcept2.first(where: { $0.strength == 0 || $0.strength == 1 })
-        }
-        
-        if focus2ID != 0,
-            userConcept1.strength == 1,
-            let userConcept2 = userConcept2,
-            (userConcept1.conceptID == focus1ID || userConcept1.conceptID == focus2ID),
-            (userConcept2.conceptID != focus1ID && userConcept2.conceptID != focus2ID)
-        {
-            return PracticeOneConceptLearningStep(conceptID: userConcept1.conceptID)
+        if let nextStep = learningStepForContinuedTwoConceptPractice(userConcept1: userConcept1) {
+            return nextStep
         }
         
         if userConcept1.strength == 0 {
             return ConceptIntroLearningStep(conceptID: 1)
         }
-        
         
         if userConcept1.strength == 1, let userConcept2 = userConcept2 {
             
@@ -66,6 +74,20 @@ class NewMaterialLearningStepStrategy: LearningStepStrategy {
         }
         
         return ConceptIntroLearningStep(conceptID: 1)
+    }
+    
+    private func learningStepForContinuedTwoConceptPractice(userConcept1: UserConcept) -> LearningStep? {
+        if userConcept1.strength == 1,
+            userConceptIsContinuedFromTwoConceptPractice(userConcept1)
+        {
+            return PracticeOneConceptLearningStep(conceptID: userConcept1.conceptID)
+        } else {
+            return nil
+        }
+    }
+    
+    private func userConceptIsContinuedFromTwoConceptPractice(_ userConcept: UserConcept) -> Bool {
+        return (userConcept.conceptID == focus1ID || userConcept.conceptID == focus2ID)
     }
     
 }
