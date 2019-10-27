677aab6c44b69dce7f5490cd5e11aa509ec18441
Peter Cerhan <peter@foliahealth.com>
2019-10-27 16:08:34 -0400

    Remove feed package structure

--- MathAppTests/FeedSequence/Models/FeedPackage+stubs.swift
+++ MathAppTests/FeedSequence/Models/FeedPackage+stubs.swift
@@ -9,33 +9,33 @@
 import Foundation
 @testable import MathApp
 
-extension FeedPackage {
-
-    static func createExercisesStub(exercises: [Exercise]) -> FeedPackage {
-        return FeedPackage(feedPackageType: .exercises, exercises: exercises, transitionItem: nil)
-    }
-    
-    
-    static var exercisesPackage: FeedPackage {
-        let exercises = [Exercise.exercise1, Exercise.exercise2, Exercise.exercise3]
-        return FeedPackage(feedPackageType: .exercises, exercises: exercises, transitionItem: nil)
-    }
-    
-    static var exercisePackage2: FeedPackage {
-        let exercises = [Exercise.exercise4, Exercise.exercise5, Exercise.exercise6]
-        return FeedPackage(feedPackageType: .exercises, exercises: exercises, transitionItem: nil)
-    }
-    
-//    static var constantRuleIntro: FeedPackage {
-//        let exercises = [Exercise.exercise1, Exercise.exercise2, Exercise.exercise3]
-//        let transitionItem = ConceptIntro(concept: Concept.constantRule)
-//        return FeedPackage(feedPackageType: .conceptIntro, exercises: exercises, transitionItem: transitionItem)
+//extension FeedPackage {
+//
+//    static func createExercisesStub(exercises: [Exercise]) -> FeedPackage {
+//        return FeedPackage(feedPackageType: .exercises, exercises: exercises, transitionItem: nil)
 //    }
-    
-//    static var constantRuleLevelUp: FeedPackage {
+//    
+//    
+//    static var exercisesPackage: FeedPackage {
 //        let exercises = [Exercise.exercise1, Exercise.exercise2, Exercise.exercise3]
-//        let transitionItem = LevelUpItem(concept: Concept.constantRule, previousLevel: 0, newLevel: 1)
-//        return FeedPackage(feedPackageType: .levelUp, exercises: exercises, transitionItem: transitionItem)
+//        return FeedPackage(feedPackageType: .exercises, exercises: exercises, transitionItem: nil)
 //    }
-
-}
+//    
+//    static var exercisePackage2: FeedPackage {
+//        let exercises = [Exercise.exercise4, Exercise.exercise5, Exercise.exercise6]
+//        return FeedPackage(feedPackageType: .exercises, exercises: exercises, transitionItem: nil)
+//    }
+//    
+////    static var constantRuleIntro: FeedPackage {
+////        let exercises = [Exercise.exercise1, Exercise.exercise2, Exercise.exercise3]
+////        let transitionItem = ConceptIntro(concept: Concept.constantRule)
+////        return FeedPackage(feedPackageType: .conceptIntro, exercises: exercises, transitionItem: transitionItem)
+////    }
+//    
+////    static var constantRuleLevelUp: FeedPackage {
+////        let exercises = [Exercise.exercise1, Exercise.exercise2, Exercise.exercise3]
+////        let transitionItem = LevelUpItem(concept: Concept.constantRule, previousLevel: 0, newLevel: 1)
+////        return FeedPackage(feedPackageType: .levelUp, exercises: exercises, transitionItem: transitionItem)
+////    }
+//
+//}
